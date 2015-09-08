b71cfc1ed214a6082464e879d281a30bf71f139e
krewast <bastiankres@gmail.com>
2015-09-08 20:48:06 +0200

    Umbenannt, Lesbarkeit erhöht, Kommentare hinzugefügt

--- Zu 03 - Operatoren, Schleifen und Verzweigungen/3.3 - Gleichheit Arrays/README.md
+++ Zu 03 - Operatoren, Schleifen und Verzweigungen/3.3 - Gleichheit Arrays/README.md
@@ -1,5 +1,7 @@
 #3.3 - Gleichheit Arrays
 
-Eine Funktion f(int[],int[]), die zwei eindimensionale int-Arrays als Parameter übergeben bekommt und überprüft, ob die darin gespeicherten Werte jeweils gleich sind
+Eine Funktion arraysEqual(int[],int[]), die zwei eindimensionale int-Arrays als Parameter übergeben bekommt und überprüft, ob die darin gespeicherten Werte jeweils gleich sind.
 
-Beispiel: f([1,4,3], [1, 4, 3]) == true
+Beispiel:
+
+	arraysEqual([1,4,3], [1, 4, 3]) == true
--- Zu 03 - Operatoren, Schleifen und Verzweigungen/3.3 - Gleichheit Arrays/src/GleichheitArrays.java
+++ Zu 03 - Operatoren, Schleifen und Verzweigungen/3.3 - Gleichheit Arrays/src/GleichheitArrays.java
@@ -1,33 +1,30 @@
-package Aufgabe4;
+public class GleichheitArrays {
+    public static void main(String[] args) {
+        // Initalisieren beider eindimensionalen int-Arrays
+        int[] arr1 = {1,2,4};
+        int[] arr2 = {1,2,4};
+        
+        // Überprüfen der beiden Arrays auf Gleichheit mit Hilfe der Funktion
+        if(arraysEqual(arr1,arr2)) {
+            System.out.println("Arrays stimmen überein");
+        } else {
+            System.out.println("Arrays stimmen nicht überein");
+        }
+    }
 
-public class Aufgabe4 {
-
-	public static void main(String[] args) {
-		//Initalisieren beider eindimensionalen int-Arrays
-		int[] arr1 = {1,2,4};
-		int[] arr2 = {1,2,4};
-		
-		//Überprüfen der beiden Arrays auf Gleichheit
-		if(!f(arr1,arr2)) {
-			System.out.println("Arrays stimmen nicht überein");
-		} else {
-			System.out.println("Arrays stimmen überein");
-		}
-	}
-	
-	
-	public static boolean f(int[] arr1, int[] arr2) {
-		//Falls eines der beiden Arrays leer ist oder die Länge der beiden unterschiedlich, wird false zurückgegeben
-		if(arr1 == null || arr2 == null || arr1.length != arr2.length) {
-			return false;
-		} else {
-			for(int i=0; i!=arr1.length; i++) {
-				if(arr1[i]!=arr2[i]) {
-					return false;
-				}
-			}
-			return true;
-		}
-	}
-
+    public static boolean arraysEqual(int[] arr1, int[] arr2) {
+        //Falls eines der beiden Arrays leer ist oder die Länge der beiden unterschiedlich, wird false zurückgegeben
+        if(arr1 == null || arr2 == null || arr1.length != arr2.length) {
+            return false;
+        } else {
+            for(int i = 0; i < arr1.length; i++) {
+                if(arr1[i] != arr2[i]) {
+                    // Wenn auch nur ein Element in einem der Arrays anders ist, gibt die Funktion sofort "false" zurück
+                    return false;
+                }
+            }
+            // Wenn alle Elemente gegenseitig überprüft wurden, und gleich sind, gibt die Funktion "true" zurück
+            return true;
+        }
+    }
 }
