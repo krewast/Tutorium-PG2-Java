a835fc1489fb9b3e3ba1f6cbc3b309053ed803b3
krewast <bastiankres@gmail.com>
2015-09-08 20:52:34 +0200

    Umbenannt, Fehlende Funktionalität laut Anforderung ergänzt, Kommentare hinzugefügt, kleinere Bugfixes

--- Zu 03 - Operatoren, Schleifen und Verzweigungen/3.6 - Notenliste/src/Notenliste.java
+++ Zu 03 - Operatoren, Schleifen und Verzweigungen/3.6 - Notenliste/src/Notenliste.java
@@ -1,44 +1,47 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
+public class Notenliste {
+    public static void main(String[] args) {
+        String[][] students = new String[0][2]; // Zweidimensionales Array mit akutell 0 Reihen, und 2 Spalten (Spalte 1: Name, Spalte 2: Note)
+        int studentCounter = 0;
+        boolean addAnotherStudent;
         Scanner sc = new Scanner(System.in);
 
-        int c = 0;
-
-        String[][] teilnehmer = new String[0][2];
-
-        while (true)
-        {
-            System.out.print("Weiteren Kursteilnehmer erfassen (0/1)? ");
-
-            if (sc.nextInt() == 0)
-                break;
-
+        do  {
             // Siehe Vorlesung 02_Datentypen_Variablen_Kodierung_Binaerzahlen_Arrays Seite 60
-            teilnehmer = Arrays.copyOf(teilnehmer, teilnehmer.length + 1);
-            teilnehmer[c] = new String[2];
+            students = Arrays.copyOf(students, students.length + 1);
+            students[studentCounter] = new String[2];
 
-            // Workaround zu Java Problem
-            // Konsole gibt \n von sc.nextInt() an sc.nextLine() weiter
-            // Das führt dazu, dass sc.nextLine() übersprungen zu werden scheint.
-            sc.nextLine();
-
+            // Name einlesen
             System.out.print("Name: ");
-            teilnehmer[c][0] = sc.nextLine();
+            students[studentCounter][0] = sc.nextLine();
+
+            // Note einlesen und validieren
             System.out.print("Note: ");
-            teilnehmer[c][1] = Integer.toString(sc.nextInt());
+            while (true) {
+                int grade = sc.nextInt();
+                if (grade > 0 && grade < 7) {
+                    students[studentCounter][1] = Integer.toString(grade);
+                    break;
+                } else {
+                    System.out.println("Bitte eine Note im Bereich zwischen 1 und 6 eingeben.");
+                }
+            }
 
-            c++;
-        }
+            studentCounter++;
 
-        for (int t = 0; t < teilnehmer.length; t++)
-        {
-            System.out.println(String.format("%d. %s:\t%d", t+1,teilnehmer[t][0],
-                    Integer.parseInt(teilnehmer[t][1]) -1 ));
+            System.out.print("Weiteren Kursstudents erfassen (0/1)? ");
+            addAnotherStudent = (sc.nextInt() == 1);
+
+            // Workaround zu Java Problem:
+            // Konsole gibt \n (newline) von sc.nextInt() an sc.nextLine() im neuen Durchlauf der while-Schleife weiter
+            // Das führt dazu, dass sc.nextLine() übersprungen zu werden scheint. Zum Testen die folgende Zeile einfach auskommentieren und probieren.
+            sc.nextLine();
+        } while(addAnotherStudent);
+
+        for (int i = 0; i < students.length; i++) {
+            System.out.println(String.format("%d. %s:\t%d", i + 1, students[i][0], Integer.parseInt(students[i][1]) - 1));
         }
     }
 }
