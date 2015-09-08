bd7cc6ad1914afe933fab60dc86a64220dc55fc2
krewast <bastiankres@gmail.com>
2015-09-08 20:46:58 +0200

    Umbenannt, Vereinheitlicht, Kommentare hinzugefügt

--- Zu 03 - Operatoren, Schleifen und Verzweigungen/3.2 - Schleife/README.md
+++ Zu 03 - Operatoren, Schleifen und Verzweigungen/3.2 - Schleife/README.md
@@ -1,6 +1,6 @@
 #3.2 - Schleife
 
-Schreiben Sie ein Java-Programm, das zunächst die Eingabe eines positiven int-Wertes max und eines positiven int-Wertes div über die Tastatur erwartet. Anschließend soll das Programm alle Zahlen zwischen 0 und max, die sich ohne Rest durch div dividieren lassen, auf den Bildschirm ausgeben.
+Schreiben Sie ein Java-Programm, das zunächst die Eingabe eines positiven int-Wertes "max" und eines positiven int-Wertes "div" über die Tastatur erwartet. Anschließend soll das Programm alle Zahlen zwischen 0 und max, die sich ohne Rest durch div dividieren lassen, auf den Bildschirm ausgeben.
 
 Beispiel:
 
@@ -10,5 +10,6 @@ Beispiel:
     div:  9
 
 *Ausgabe:*
+
     0
     9
--- Zu 03 - Operatoren, Schleifen und Verzweigungen/3.2 - Schleife/src/Schleife.java
+++ Zu 03 - Operatoren, Schleifen und Verzweigungen/3.2 - Schleife/src/Schleife.java
@@ -1,36 +1,35 @@
-package Aufgabe3;
-
 import java.util.Scanner;
 
-public class Aufgabe3 {
+public class Schleife {
+    public static void main(String[] args) {
+        int max, div;
+        Scanner sc = new Scanner(System.in);
+        
+        // "max" einlesen
+        System.out.print("max: ");
+        do {
+            max = sc.nextInt();
+            if (max < 0){
+                System.out.println("Bitte eine positive Zahl eingeben: ");
+            }
+        } while (max < 0);
 
-	public static void main(String[] args) {
-		int max, div;
-		//Scanner initialisieren
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("max:");
-		do {
-			max = sc.nextInt();
-			if (max < 0){
-				System.out.println("Bitte eine nicht-negative Zahl eingeben:");
-			}
-		} while (max < 0);
-
-		System.out.println("div:");
-		do {
-			div = sc.nextInt();
-			if (div < 0){
-				System.out.println("Bitte eine nicht-negative Zahl eingeben:");
-			}
-		} while (div < 0);
-		
-		for(int i = 0; i<=max; i++) {
-			//Falls sich i durch div restlos teilen lässt, wird der aktuelle Wert von i über die Konsole ausgegeben
-			if((i%div)==0) {               
-				System.out.println(i);
-			}
-		}
-	}
-
+        // "div" einlesen
+        System.out.print("div: ");
+        do {
+            div = sc.nextInt();
+            if (div < 0){
+                System.out.println("Bitte eine positive Zahl eingeben: ");
+            }
+        } while (div < 0);
+        
+        System.out.println("--- Ergebnis ---");
+        // Von 0 bis max iterieren
+        for (int i = 0; i <= max; i++) {
+            //Falls sich i durch div restlos teilen lässt, wird der aktuelle Wert von i über die Konsole ausgegeben
+            if((i % div) == 0) {               
+                System.out.println(i);
+            }
+        }
+    }
 }
