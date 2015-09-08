5b1ad4b475e9cd6c05abb4b8374e78e160906f5a
krewast <bastiankres@gmail.com>
2015-09-08 20:49:24 +0200

    Umbenannt, kleinere Verbesserungen

--- Zu 03 - Operatoren, Schleifen und Verzweigungen/3.4 - Wechselkursrechner/README.md
+++ Zu 03 - Operatoren, Schleifen und Verzweigungen/3.4 - Wechselkursrechner/README.md
@@ -1,10 +1,10 @@
 #3.4 - Wechselkursrechner
 
-Schreiben Sie ein Java-Programm, das die Anzahl der Euro-Geldstücke und Euro-Geldscheine vom Benutzer abfragt, den gesamten so erfassten Euro-Betrag in US-Dollar umrechnet und das USD-Ergebnis anzeigt.
+Schreiben Sie ein Java-Programm, das die Anzahl der Euro-Geldstücke die der Benutzer besitzt abfragt, den gesamten so erfassten Euro-Betrag in US-Dollar umrechnet und das USD-Ergebnis anzeigt.
 
 Entwerfen Sie einen geeigneten Weg, die nötigen Eingaben in der Command-Line abzufragen. Nutzen Sie hierfür wiederum die Methoden
 
     Scanner sc = new Scanner(System.in);
-    float zahl = sc.nextFloat();
+    float zahl = sc.nextInt();
 
 Einen Aktuellen Wechselkurs finden Sie [hier auf finanzen.net](http://www.finanzen.net/devisen/dollarkurs)
--- Zu 03 - Operatoren, Schleifen und Verzweigungen/3.4 - Wechselkursrechner/src/Wechselkursrechner.java
+++ Zu 03 - Operatoren, Schleifen und Verzweigungen/3.4 - Wechselkursrechner/src/Wechselkursrechner.java
@@ -1,24 +1,19 @@
 import java.util.Scanner;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
-        // Wechselkurs 1.2756 Dollar = 1 Euro
-        double course = 1.2756;
-        // Euro cent coins
-        int[] coins = { 1, 2, 5, 10, 20, 50, 100, 200};
-
+public class Wechselkursrechner {
+    public static void main(String[] args) {
+        // Wechselkurs 1 Euro == 1.2756 Dollar
+        double exchangeRate = 1.2756;
+        // Euro Cent coins
+        int[] coins = {1, 2, 5, 10, 20, 50, 100, 200};
         int totalCents = 0;
         Scanner sc = new Scanner(System.in);
 
-        for (int i = 0; i < coins.length; i++)
-        {
-            System.out.print(String.format("Wie viele %d Münzen haben Sie: ", coins[i]));
+        for (int i = 0; i < coins.length; i++) {
+            System.out.print(String.format("Wie viele %3d-Cent Münzen haben Sie? ", coins[i]));
             totalCents += sc.nextInt() * coins[i];
         }
 
-        System.out.println(String.format("Sie haben %.2f Euro, das entspricht %.2f Dollar.",
-                totalCents / 100f, totalCents * course / 100f));
+        System.out.println(String.format("Sie haben %.2f Euro, das entspricht %.2f Dollar.", totalCents / 100f, totalCents * exchangeRate / 100f));
     }
 }
