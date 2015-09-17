841bbb1feea951c517a59d9382c1990b8a0ff7c8
krewast <bastiankres@gmail.com>
2015-09-17 23:28:37 +0200

    Bug mit den Zufallszahlen gefixed, Code aufgeräumt, Kommentare hinzugefügt, Beschreibung übersichtlicher gestaltet

--- Zu 03 - Operatoren, Schleifen und Verzweigungen/3.9 - Roulette/src/Roulette.java
+++ Zu 03 - Operatoren, Schleifen und Verzweigungen/3.9 - Roulette/src/Roulette.java
@@ -1,69 +1,71 @@
-package pg2._04;
-
 import java.util.Scanner;
 
 public class Roulette {
+    public static void main(String[] args) {
+        // "Initialisierung" des Spiels
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Willkommen beim Roulette");
+        int playerMoney = 1000;
 
-	static int seedFunding = 1000;
+        // Check, ob noch Geld zur Verfügung steht
+        while (playerMoney > 0) {
+            System.out.println("Ihr aktuelles Guthaben beträgt " + playerMoney + " Euro.");
+            System.out.print("Bitte setzen Sie einen Betrag für Ihr nächstes Spiel: ");
+            int bet = sc.nextInt();
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
+            // Check, ob gesetzter Betrag kleiner oder gleich dem verbleibenden Geld des Spielers
+            if (bet <= playerMoney) {
+                System.out.print("Bitte setzen Sie auf eine Farbe (r/s) oder eine Zahl (1-35): ");
+                String choice = sc.next();
 
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Willkommen beim Roulette Spiel");
+                // Gewählten Einsatz vom Guthaben des Spielers abziehen
+                playerMoney -= bet;
 
-		while (seedFunding > 0) {
-			System.out.println("Ihr aktuelles Guthaben beträgt " + seedFunding
-					+ " Euro.");
-			System.out
-					.println("Setzten Sie einen Betrag für Ihr nächstes Spiel:");
-			int use = sc.nextInt();
-			if (use <= seedFunding) {
+                // Spielen
+                int random = getRandomNumber();
+                boolean playerWon = false;
+                if (choice.equals("r")) {
+                    // Wenn gerade Zahl, dann rot
+                    if (random % 2 == 0) {
+                        System.out.println(random + " - Rot gewinnt, Glückwunsch");
+                        playerMoney += bet * 2;
+                        playerWon = true;
+                    }
+                } else if (choice.equals("s")) {
+                    // Wenn ungerade Zahl, dann schwarz
+                    if (random % 2 != 0) {
+                        System.out.println(random + " - Schwarz gewinnt, Glückwunsch");
+                        playerMoney += bet * 2;
+                        playerWon = true;
+                    }
+                } else if ((Integer.valueOf(choice) >= 1 && Integer.valueOf(choice) <= 36)) {
+                    if (random == Integer.valueOf(choice)) {
+                        System.out.println(random + " - Sie gewinnen, Glückwunsch");
+                        playerMoney += bet * 35;
+                        playerWon = true;
+                    }
+                } else {
+                    System.err.println("Ungueltige Auswahl, neu beginnen!");
+                }
 
-				System.out
-						.println("Setzten Sie auf eine Farbe(r/s) oder eine Zahl(1-35)");
-				String choice = sc.next();
-				seedFunding -= use;
-				if (( choice.equals("r"))) {
+                if (!playerWon) {
+                    System.out.println(random + " - Leider verloren");
+                }
+            } else {
+                System.err.println("Der eingesetzte Betrag übersteigt Ihr Guthaben. Bitte neu setzen.");
+            }
 
-					int random = myRandom();
-					
-					if (random%2 == 0){
-						seedFunding += use * 2;
-					}
-				} else if (choice.equals("s")){
+            System.out.println("--------------------");
+        }
+        System.out.println("Vielen Dank für Ihren Besuch");
+    }
 
-					int random = myRandom();
-					
-					if (random%2 != 0){
-						seedFunding += use * 2;
-					}
-				}
-				else if ((Integer.valueOf(choice) >= 1 && Integer.valueOf(choice) <= 36)){
-
-					int random = myRandom();
-					
-					if (random == Integer.valueOf(choice)){
-						seedFunding += use * 35;
-					}
-				} else {
-					System.err.println("Falsche Auswahl! Neu beginnen!");
-				}
-			} else {
-				System.err
-						.println("Eingesetzte Betrag zu hoch - neuen eingeben");
-			}
-		}
-	}
-
-	public static int myRandom() {
-		int start = 1;
-		int end = 36;
-		int random = (int) (Math.random() * (end - start) + start);
-		System.out.println("Roulette - Ergebnis: " + random);
-		return random;
-	}
-
+    public static int getRandomNumber() {
+        // Achtung: Zufallszahlen richtig hinzubekommen ist gar nicht immer so leicht wie es vielleicht scheint
+        // Eine schöne Anleitung findet sich hier: http://stackoverflow.com/a/363732
+        int min = 1;
+        int max = 36;
+        int randomInteger = min + (int)(Math.random() * ((max - min) + 1));
+        return randomInteger;
+    }
 }
