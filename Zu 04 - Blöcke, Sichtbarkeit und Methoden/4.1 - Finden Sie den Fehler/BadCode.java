2e913c72ea6d2b7e3db2500fe2945ee575ae6992
krewast <bastiankres@gmail.com>
2015-09-19 22:39:13 +0200

    Code der Aufgabenstellung und Lösung angeglichen, Übersichtlichkeit erhöht, Kommentare vereinheitlicht

--- Zu 04 - Blöcke, Sichtbarkeit und Methoden/4.1 - Finden Sie den Fehler/BadCode.java
+++ Zu 04 - Blöcke, Sichtbarkeit und Methoden/4.1 - Finden Sie den Fehler/BadCode.java
@@ -1,41 +1,36 @@
-import java.util.Scanner
+import java.util.Scanner;
 
-public class Section4 {
+public class BadCode {
+    int start = 1;
 
-  int start = 1;
-  
-  public static void main(int[] args) {
+    public static void main(int[] args) {
+        if (args != null){
+            start = args[0]
+        }
 
-    if (args != null){
-      start = args[0]
+        System.out.println("Die Ursprungszahl ist: " + start);
+        Scanner sc = new Scanner(System.in);
+        double d1 = add(start, sc.nextInt());   
+        double d2 = sub(start, sc.nextFloat());
+
+        test(20);
     }
-    
-    System.out.println("Die Ürsprungszahl ist: " + start);
-    Scanner sc = new Scanner(System.in);
-    double d1 = add(start, sc.nextInt());   
-    double d2 = sub(start, sc.nextFloat());
 
-    test(20);
-    
-  }
-  
-  public static void add(int start, int add){
-    return (start + add);
-  }
-  
-  public double sub(int start, int sub){
-    return (start - sub);
-  }
-
-  public static void test(int start){
-    if (start > 10){
-      int value = 5;
+    public static void add(int start, int add) {
+        return (start + add);
     }
-    else {
-      int value = -5;
-    }
-    
-    System.out.println(value);
-  }
 
+    public int sub(int start, int sub) {
+        return (start - sub);
+    }
+
+    public static void test(int start) {
+        if (start > 10) {
+          int value = 5;
+        } else {
+          int value = -5;
+        }
+
+        System.out.println(value);
+    }
 }
--- Zu 04 - Blöcke, Sichtbarkeit und Methoden/4.1 - Finden Sie den Fehler/BadCodeSolution.java
+++ Zu 04 - Blöcke, Sichtbarkeit und Methoden/4.1 - Finden Sie den Fehler/BadCodeSolution.java
@@ -1,42 +1,36 @@
 import java.util.Scanner;
 
-public class Section4 {
+public class BadCodeSolution {
+    int start = 1;
 
-  int start = 1;
-  
-  public static void main(int[] args) {
+    public static void main(int[] args) {
+        if (args != null){
+            start = args[0]                                     // Fehlendes Semikolon
+        }
 
-    if (args != null){
-      start = args[0]  //Fehlender Strichpunkt
-    }
-    
-    System.out.println("Die Ursprungszahl ist: " + start);  //Variable "start" nicht vorhanden (static)
-    Scanner sc = new Scanner(System.in);
-        
-    double d1 = add(start, sc.nextInt()); //Variable "start" nicht vorhanden (static)
-    
-    double d2 = sub(start, sc.nextFloat());  //Float statt Integer als Übergabewert
-    
-    test(20);
-  }
-  
-  public static void add(int start, int add){
-    return (start + add);  //Falscher Rückgabewert der Funktion
-  }
-  
-  public int sub(int start, int sub){ //Aufruf der Funktion nicht möglich (static)
-    return (start - sub);
-  }
-  
-  public static void test(int start){
-    if (start > 10){
-      int value = 5;
-    }
-    else {
-      int value = -5;
-    }
-    
-    System.out.println(value);  //Der Lebensraum der Variable "value" endet mit dem Block der if-Bedinung
-  }
+        System.out.println("Die Ursprungszahl ist: " + start);  // "start" unbekannt: Variable "start" nicht bekannt, da "static" fehlt - Richtig: static int start = 1;
+        Scanner sc = new Scanner(System.in);
+        double d1 = add(start, sc.nextInt());                   // "start" unbekannt
+        double d2 = sub(start, sc.nextFloat());                 // "start" unbekannt; Float statt Integer als Übergabewert
 
+        test(20);
+    }
+
+    public static void add(int start, int add) {                // Falscher Rückgabewert "void" statt "int" der Methode
+        return (start + add);
+    }
+
+    public int sub(int start, int sub) {                        // Aufruf der Methode nicht möglich, da "static" fehlt
+        return (start - sub);
+    }
+
+    public static void test(int start) {
+        if (start > 10) {
+          int value = 5;
+        } else {
+          int value = -5;
+        }
+
+        System.out.println(value);                              // "value" ist hier unbekannt, da sie innerhalb der if/else Blöcke deklariert wurde
+    }
 }
