b49982d82f93a52c9455d037a170c822eb39eb7d
mHollweck <mariahollweck@gmail.com>
2015-10-12 13:26:47 +0200

    [05][Studentenverwaltung] Updated spacing (4)

--- Zu 05 - Grundlagen der Objektorientierung/5.1 - Studentenverwaltung/src/Class.java
+++ Zu 05 - Grundlagen der Objektorientierung/5.1 - Studentenverwaltung/src/Class.java
@@ -1,7 +1,5 @@
 // search for #learnmore if you want to learn some java tricks!
 
-
-
 /*
  * This class called 'Class' manages the class. It holds the
  * values 'credit' (e.g. 2), multiplier (e.g. 3), name (e.g. PG2) and grade.
@@ -9,51 +7,48 @@
  */
 public class Class {
 
-  // #learnmore naming convention: class variables with underscore _
-  private int _credits;
-  private int _multiplier;
-  private String _name;
-  private double _grade;
+    // #learnmore naming convention: class variables with underscore _
+    private int _credits;
+    private int _multiplier;
+    private String _name;
+    private double _grade;
 
-  // setter
-  public void setCredits(int credits) {
-    this._credits = credits;
-  }
+    // setter
+    public void setCredits(int credits) {
+        this._credits = credits;
+    }
 
-  public void setMultiplier(int multiplier) {
-    this._multiplier = multiplier;
-  }
+    public void setMultiplier(int multiplier) {
+        this._multiplier = multiplier;
+    }
 
-  public void setName(String name) {
-    this._name = name;
-  }
+    public void setName(String name) {
+        this._name = name;
+    }
 
-  public void setGrade(double grade) {
-    this._grade = grade;
-  }
+    public void setGrade(double grade) {
+        this._grade = grade;
+    }
 
-  // getter
-  public int getCredits() {
-    return this._credits;
-  }
+    // getter
+    public int getCredits() {
+        return this._credits;
+    }
 
-  public int getMultiplier() {
-    return this._multiplier;
-  }
+    public int getMultiplier() {
+        return this._multiplier;
+    }
 
-  public String getName() {
-    return this._name;
-  }
+    public String getName() {
+        return this._name;
+    }
 
-  public double getGrade() {
-    return this._grade;
-  }
+    public double getGrade() {
+        return this._grade;
+    }
 
-  public String getClassData() {
-    return  
-      "  Name: " + this._name + "\n" +
-      "  Credit: " + this._credits + "\n" +
-      "  Multiplikator: " + this._multiplier + "\n" +
-      "  Grade: " + String.format("%10.2f", this._grade) + "\n";
-  }
+    public String getClassData() {
+        return "  Name: " + this._name + "\n" + "  Credit: " + this._credits + "\n" + "  Multiplikator: "
+                + this._multiplier + "\n" + "  Grade: " + String.format("%10.2f", this._grade) + "\n";
+    }
 }
--- Zu 05 - Grundlagen der Objektorientierung/5.1 - Studentenverwaltung/src/Course.java
+++ Zu 05 - Grundlagen der Objektorientierung/5.1 - Studentenverwaltung/src/Course.java
@@ -1,3 +1,4 @@
+
 // search for #learnmore if you want to learn some java tricks!
 
 import java.util.ArrayList;
@@ -10,60 +11,60 @@ import java.util.List;
  */
 public class Course {
 
-  // #learnmore naming convention: class variables with underscore _
-  private List<Class> _classes = new ArrayList<Class>();
-  private String _name; // e.g. Computer Science
-  private String _degree; //e.g. Bachelor of Science
+    // #learnmore naming convention: class variables with underscore _
+    private List<Class> _classes = new ArrayList<Class>();
+    private String _name; // e.g. Computer Science
+    private String _degree; // e.g. Bachelor of Science
 
-  public Course() {}
-
-  public Course(List<Class> classes, String name, String degree) {
-    this._classes = classes;
-    this._name = name;
-    this._degree = degree;
-  }
-
-  //setter
-  public void setClasses(List<Class> classes) {
-    this._classes = classes;
-  }
-
-  /* #learnmore
-   * make your code look well organized! check out the 'setName' function
-   * and ask yourself why this code style is not what we want. (compare it to
-   * the other functions).
-   */
-  void   setName (String  str ){
-    this._name =    str ;
-
-  }
-
-  public void setDegree(String degree) {
-    this._degree = degree;
-  }
-
-  //getter
-  public List<Class> getClasses() {
-    return this._classes;
-  }
-
-  public String getName() {
-    return this._name;
-  }
-
-  public String getDegree() {
-    return this._degree;
-  }
-
-  public void addClass(Class myclass) {
-    this._classes.add(myclass);
-  }
-
-  public String getCoursesData() {
-    String data = "";
-    for (Class myclass : this._classes){
-      data += myclass.getClassData() + "\n---------\n";
+    public Course() {
     }
-    return data;
-  }
+
+    public Course(List<Class> classes, String name, String degree) {
+        this._classes = classes;
+        this._name = name;
+        this._degree = degree;
+    }
+
+    // setter
+    public void setClasses(List<Class> classes) {
+        this._classes = classes;
+    }
+
+    /*
+     * #learnmore make your code look well organized! check out the 'setName'
+     * function and ask yourself why this code style is not what we want.
+     * (compare it to the other functions).
+     */
+    void setName(String str) {
+        this._name = str;
+    }
+
+    public void setDegree(String degree) {
+        this._degree = degree;
+    }
+
+    // getter
+    public List<Class> getClasses() {
+        return this._classes;
+    }
+
+    public String getName() {
+        return this._name;
+    }
+
+    public String getDegree() {
+        return this._degree;
+    }
+
+    public void addClass(Class myclass) {
+        this._classes.add(myclass);
+    }
+
+    public String getCoursesData() {
+        String data = "";
+        for (Class myclass : this._classes) {
+            data += myclass.getClassData() + "\n---------\n";
+        }
+        return data;
+    }
 }
