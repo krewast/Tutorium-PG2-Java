77d633b5cdd4feec0891dd877bef2e27c6b5b872
mHollweck <mariahollweck@gmail.com>
2015-11-02 20:48:05 +0100

    [06][Formenverwaltung] Updated code and cleaned up

--- Zu 06 - Vererbung, Sichtbarkeit, this, Konstruktor/6.1 - Formenverwaltung/src/Rectangular.java
+++ Zu 06 - Vererbung, Sichtbarkeit, this, Konstruktor/6.1 - Formenverwaltung/src/Rectangular.java
@@ -5,39 +5,43 @@ package de.oth.pg2_4.aufgabe_1;
 // additional items (here width)
 public class Rectangular extends Square {
 
-	private double _width;
-	
-	/**
-	 * Konstruktor der Klasse
-	 * @param lenght
-	 * @param width
-	 */
-	public Rectangular(double length, double width) {
-		//Ruft Konstruktor von Quadrat auf
-		super(length);
-		this._width = width;
-	}	
-	
-	/**
-	 * Berechnet den Umfang des Rechtecks
-	 * @return Umfang des Rechtecks
-	 */
-	public double berechneUmfang(){
-		return 2 * this.getWidth() + 2 * this.getLength();
-	}
-	
-	/**
-	 * Berechnet die Fl�che des Rechtecks
-	 * @return Fl�che des Rechtecks
-	 */
-	public double berechneFlaeche(){
-		return this.getLength() * this.getWidth();
-	}
-	
-	public double getWidth() {
-		return this._width;
-	}
-	public void setWidth(double width) {
-		this._width = width;
-	}
+    private double _width;
+
+    /**
+     * Konstruktor der Klasse
+     * 
+     * @param lenght
+     * @param width
+     */
+    public Rectangular(double length, double width) {
+        // Ruft Konstruktor von Quadrat auf
+        super(length);
+        this._width = width;
+    }
+
+    /**
+     * Berechnet den Umfang des Rechtecks
+     * 
+     * @return Umfang des Rechtecks
+     */
+    public double berechneUmfang() {
+        return 2 * this.getWidth() + 2 * this.getLength();
+    }
+
+    /**
+     * Berechnet die Fl�che des Rechtecks
+     * 
+     * @return Fl�che des Rechtecks
+     */
+    public double berechneFlaeche() {
+        return this.getLength() * this.getWidth();
+    }
+
+    public double getWidth() {
+        return this._width;
+    }
+
+    public void setWidth(double width) {
+        this._width = width;
+    }
 }
--- Zu 06 - Vererbung, Sichtbarkeit, this, Konstruktor/6.1 - Formenverwaltung/src/Square.java
+++ Zu 06 - Vererbung, Sichtbarkeit, this, Konstruktor/6.1 - Formenverwaltung/src/Square.java
@@ -1,39 +1,42 @@
 package de.oth.pg2_4.aufgabe_1;
 
-public class Square{
-    
-	private double _length;
+public class Square {
 
-	/**
-	 * Konstruktor der Klasse
-	 * @param d
-	 */
-	public Square(double d){
-		this._length = d;
-	}
-	
-	/**
-	 * Berechnet den Umfang des Quadrats
-	 * @return Umfang des Quadrats
-	 */
-	public double berechneUmfang(){
-		return 4 * this._length;
-	}
-	
-	/**
-	 * Berechnet den Fl�cheninhalt des Quadrats
-	 * @return Fl�che des Quadrats
-	 */
-	public double berechneFlaeche(){
-		return this._length * this._length;
-	}
-	
-	public double getLength() {
-		return this._length;
-	}
+    private double _length;
 
-	public void setLength(double length) {
-		this._length = length;
-	}
-	
+    /**
+     * Konstruktor der Klasse
+     * 
+     * @param d
+     */
+    public Square(double d) {
+        this._length = d;
+    }
+
+    /**
+     * Berechnet den Umfang des Quadrats
+     * 
+     * @return Umfang des Quadrats
+     */
+    public double berechneUmfang() {
+        return 4 * this._length;
+    }
+
+    /**
+     * Berechnet den Fl�cheninhalt des Quadrats
+     * 
+     * @return Fl�che des Quadrats
+     */
+    public double berechneFlaeche() {
+        return this._length * this._length;
+    }
+
+    public double getLength() {
+        return this._length;
+    }
+
+    public void setLength(double length) {
+        this._length = length;
+    }
+
 }
