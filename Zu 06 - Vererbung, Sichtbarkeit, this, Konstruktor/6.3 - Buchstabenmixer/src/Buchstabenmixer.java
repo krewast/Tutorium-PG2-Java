055f29ad474075ffb4584a3137ad0dba8a3983bd
mHollweck <mariahollweck@gmail.com>
2015-11-12 12:51:06 +0100

    [06][Buchstabenmixer] Updated algorithm (algo was wrong!)

--- Zu 06 - Vererbung, Sichtbarkeit, this, Konstruktor/6.3 - Buchstabenmixer/src/Buchstabenmixer.java
+++ Zu 06 - Vererbung, Sichtbarkeit, this, Konstruktor/6.3 - Buchstabenmixer/src/Buchstabenmixer.java
@@ -4,29 +4,20 @@ import java.util.Scanner;
 
 public class Buchstabenmixer {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Bitte geben Sie ein Wort ein:");
-		String myString = sc.next();
-		
-		System.out.println("Neues Wort:" + mix(myString));
-		
-	}
-	
-	public static String mix(String myString){
-		
-		String newString = "";
-		
-		for (int i = 0; i < myString.length()-1; i++){
-			newString += myString.substring(i+1,i+2);
-			
-		}
-		newString += myString.substring(0,1);
-		
-		return newString;
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bitte geben Sie ein Wort ein:");
+        String myString = sc.next();
+        System.out.println("Neues Wort:" + mix(myString));
+    }
+    
+    public static String mix(String myString) {
+        int last = myString.length();
+        int middle = last / 2;
+        
+        String s_first = myString.substring(middle, last);
+        String s_last = myString.substring(0, middle);
 
+        return s_first + s_last;
+    }
 }
