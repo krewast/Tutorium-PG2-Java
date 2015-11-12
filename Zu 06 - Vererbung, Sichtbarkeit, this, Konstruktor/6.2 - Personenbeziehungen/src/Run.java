2274b40ac66af22bb546dd9e3388d784302f01f3
mHollweck <mariahollweck@gmail.com>
2015-11-12 12:39:31 +0100

    [06][Personenverwaltung] Wrote a new algorithm to find person relationships

--- Zu 06 - Vererbung, Sichtbarkeit, this, Konstruktor/6.2 - Personenbeziehungen/src/Run.java
+++ Zu 06 - Vererbung, Sichtbarkeit, this, Konstruktor/6.2 - Personenbeziehungen/src/Run.java
@@ -2,25 +2,20 @@ package de.oth.pg2.grosseUebung.aufgabe3;
 
 public class Run {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        Person person1 = new Person("Jim likes Larry and Jean, but hates Kim");
+        person1.print();
+        
+        Person person2 = new Person("Bob loves Jean, and likes Larry and Kim");
+        person2.print();
 
-		Person person1 = new Person("Jim likes Larry and Jean, but hates Kim");
-		person1.print();
-		
-		Person person2 = new Person("Bob loves Jean, and likes Larry and Kim");
-		person2.print();
-//
-		Person person3 = new Person("Jean loves Bob, likes Jim, but hates Kim");
-		person3.print();
-//		
-		Person person4 = new Person("Kim hates Jim, likes Larry and Bob");
-		person4.print();
-//		
-		Person person5 = new Person("Larry loves Martin, and hates Karl and Jean");
-		person5.print();
-	}
-
+        Person person3 = new Person("Jean loves Bob, likes Jim, but hates Kim");
+        person3.print();
+      
+        Person person4 = new Person("Kim hates Jim, likes Larry and Bob");
+        person4.print();
+      
+        Person person5 = new Person("Larry loves Martin, and hates Karl and Jean");
+        person5.print();
+    }
 }
