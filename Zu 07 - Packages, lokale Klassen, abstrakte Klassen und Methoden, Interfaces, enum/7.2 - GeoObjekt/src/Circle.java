4e04e82cd5f115e0e00dd35d9daa0c107da52f59
mHollweck <mariahollweck@gmail.com>
2015-11-12 13:04:07 +0100

    [07][GeoObject] Updated spaces

--- Zu 07 - Packages, lokale Klassen, abstrakte Klassen und Methoden, Interfaces, enum/7.2 - GeoObjekt/src/Circle.java
+++ Zu 07 - Packages, lokale Klassen, abstrakte Klassen und Methoden, Interfaces, enum/7.2 - GeoObjekt/src/Circle.java
@@ -2,20 +2,20 @@ package de.oth.pg2_6.aufgabe_1;
 
 public class Circle extends GeoObject {
 
-	public double radius;
-	
-	public Circle(double x, double y, double radius) {
-		super(x, y);
-		this.radius = radius;
-	}
+    public double radius;
 
-	@Override
-	public double berechneFlaeche() {
-		return Math.PI * radius * radius;
-	}
+    public Circle(double x, double y, double radius) {
+        super(x, y);
+        this.radius = radius;
+    }
 
-	@Override
-	public double berechneUmfang() {
-		return 2 * Math.PI * radius;
-	}
+    @Override
+    public double berechneFlaeche() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double berechneUmfang() {
+        return 2 * Math.PI * radius;
+    }
 }
--- Zu 07 - Packages, lokale Klassen, abstrakte Klassen und Methoden, Interfaces, enum/7.2 - GeoObjekt/src/Rectangular.java
+++ Zu 07 - Packages, lokale Klassen, abstrakte Klassen und Methoden, Interfaces, enum/7.2 - GeoObjekt/src/Rectangular.java
@@ -2,39 +2,43 @@ package de.oth.pg2_6.aufgabe_1;
 
 public class Rectangular extends Square {
 
-	private double width;
-	
-	/**
-	 * Konstruktor der Klasse
-	 * @param lenght
-	 * @param width
-	 */
-	public Rectangular(double x, double y, double lenght, double width) {
-		//Ruft Konstruktor von Quadrat auf
-		super(x,y,lenght);
-		this.width = width;
-	}	
-	
-	/**
-	 * Berechnet den Umfang des Rechtecks
-	 * @return Umfang des Rechtecks
-	 */
-	public double berechneUmfang(){
-		return 2 * this.getWidth() + 2 * this.getLength();
-	}
-	
-	/**
-	 * Berechnet die Fläche des Rechtecks
-	 * @return Fläche des Rechtecks
-	 */
-	public double berechneFlaeche(){
-		return this.getLength() * this.getWidth();
-	}
-	
-	public double getWidth() {
-		return width;
-	}
-	public void setWidth(double width) {
-		this.width = width;
-	}
+    private double width;
+
+    /**
+     * Konstruktor der Klasse
+     * 
+     * @param lenght
+     * @param width
+     */
+    public Rectangular(double x, double y, double lenght, double width) {
+        // Ruft Konstruktor von Quadrat auf
+        super(x, y, lenght);
+        this.width = width;
+    }
+
+    /**
+     * Berechnet den Umfang des Rechtecks
+     * 
+     * @return Umfang des Rechtecks
+     */
+    public double berechneUmfang() {
+        return 2 * this.getWidth() + 2 * this.getLength();
+    }
+
+    /**
+     * Berechnet die Flaeche des Rechtecks
+     * 
+     * @return Flaeche des Rechtecks
+     */
+    public double berechneFlaeche() {
+        return this.getLength() * this.getWidth();
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
 }
