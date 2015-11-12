db111caae4f3ddc2e915e4ad9288d11539bca6da
mHollweck <mariahollweck@gmail.com>
2015-11-12 13:15:10 +0100

    [10][Calendar] Updated spacing and styling

--- Zu 10 - Utilities: Math, Date, Calendar, System, Random/10.2 - Wochentag/src/DayOfWeek.java
+++ Zu 10 - Utilities: Math, Date, Calendar, System, Random/10.2 - Wochentag/src/DayOfWeek.java
@@ -6,9 +6,6 @@ import java.util.Scanner;
 
 public class DayOfWeek {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
@@ -21,39 +18,38 @@ public class DayOfWeek {
 		System.out.print("Jahr: ");
 		int year = sc.nextInt();
 
-		// TODO Auto-generated method stub
 		GregorianCalendar greCal = new GregorianCalendar();
 
 		greCal.set(Calendar.DATE, day);
-		greCal.set(Calendar.MONTH, month - 1); // Achtung -1, da Januar = 0
+		greCal.set(Calendar.MONTH, month - 1); // MONTH starts at 0 in Java (Januar = 0, Februar = 1, ...)
 		greCal.set(Calendar.YEAR, year);
+		
 		int dayOfWeek = greCal.get(Calendar.DAY_OF_WEEK);
-		System.out.print("Der " + day + "." + month + "." + year
-				+ " war ein ");
+		
+		System.out.print("Der " + day + "." + month + "." + year + " war ein ");
 
 		switch (dayOfWeek) {
-		case Calendar.MONDAY:
-			System.out.println("Monatag");
-			break;
-		case Calendar.TUESDAY:
-			System.out.println("Dienstag");
-			break;
-		case Calendar.WEDNESDAY:
-			System.out.println("Mittwoch");
-			break;
-		case Calendar.THURSDAY:
-			System.out.println("Donnerstag");
-			break;
-		case Calendar.FRIDAY:
-			System.out.println("Freitag");
-			break;
-		case Calendar.SATURDAY:
-			System.out.println("Samstag");
-			break;
-		case Calendar.SUNDAY:
-			System.out.println("Sonntag");
-			break;
+    		case Calendar.MONDAY:
+    			System.out.println("Montag");
+    			break;
+    		case Calendar.TUESDAY:
+    			System.out.println("Dienstag");
+    			break;
+    		case Calendar.WEDNESDAY:
+    			System.out.println("Mittwoch");
+    			break;
+    		case Calendar.THURSDAY:
+    			System.out.println("Donnerstag");
+    			break;
+    		case Calendar.FRIDAY:
+    			System.out.println("Freitag");
+    			break;
+    		case Calendar.SATURDAY:
+    			System.out.println("Samstag");
+    			break;
+    		case Calendar.SUNDAY:
+    			System.out.println("Sonntag");
+    			break;
 		}
 	}
-
 }
